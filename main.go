8b1504f2428bcef89869375f6dc1422d92d83abb
Rafael Alvarez <rafael@ubermac.local>
2020-10-08 21:45:44 -0700

    Added http site + metrics endpoint

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"gohello/metrics"
+	"net/http"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	metrics.HelloRalph()
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
 }
